57f36e5b05c52a8fbee5c04b472e0e106fe8e417
GlistValera <den_rzaev@mail.ru>
2020-10-08 19:44:12 +0300

    Адаптировал первую страницу

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,22 @@ $(document).ready(function () {
 $(document).ready(function () {
   $('.slider-two__list').bxSlider({});
 });
+
+
+// dropdown
+
+$('.footer__inf-mobile').on('click', e => {
+  e.preventDefault()
+
+  const dropDown = $('.dropdown')
+
+  dropDown.addClass('dropdown__active');
+});
+
+$('.dropdown__close').on('click', e => {
+  e.preventDefault()
+
+  const dropDown = $('.dropdown')
+
+  dropDown.removeClass('dropdown__active');
+});
